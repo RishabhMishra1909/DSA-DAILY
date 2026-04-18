fc38ff17a0683b6fa76bbb5a2d69f465b69e0531
Rishabh_19 <149862019+RishabhMishra1909@users.noreply.github.com>
2026-04-19 04:24:16 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+
+            // skip non-alphanumeric from left
+            while(left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while(left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // compare characters (case insensitive)
+            if(tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
